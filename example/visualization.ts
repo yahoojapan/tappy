05d7436b062298551af35e4122a8d9200b1d1f2b
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-08-25 16:32:25 +0900

    feat: add examples

---
+++ example/visualization.ts
@@ -0,0 +1,24 @@
+import { type Device, Tappy } from "@lycorp-jp/tappy";
+import { PuppeteerAdapter } from "@lycorp-jp/tappy/adapters";
+import { visualize } from "@lycorp-jp/tappy/utils";
+import puppeteer from "puppeteer";
+
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+
+const adapter = new PuppeteerAdapter(page);
+const tappy = new Tappy(adapter);
+
+const device: Device = {
+  name: "iPhone 12 Pro",
+  width: 390,
+  height: 844,
+  scale: 3,
+  ppi: 460,
+};
+
+const result = await tappy.analyze("https://example.com", device);
+
+visualize(device, result, "output");
+
+await browser.close();
